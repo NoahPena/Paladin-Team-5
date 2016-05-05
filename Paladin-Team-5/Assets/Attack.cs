2fbf705f399f7f3f726201fcf01bbb639f8cbdac
AndrewFrost <andrew.frost@g.austincc.edu>
2016-05-04 19:44:31 -0500

    More Merge Fixes
    Didn't realize the main menu got messed up as well; fixed it.

--- Paladin-Team-5/Assets/Attack.cs
+++ Paladin-Team-5/Assets/Attack.cs
@@ -2,6 +2,8 @@
 
 public class Attack : MonoBehaviour
 {
+	public static float enemy_Damage_Multiplier = 1.0f;
+
 	public enum attack_Type
 	{
 		regular,
@@ -27,7 +29,7 @@ public class Attack : MonoBehaviour
 		}
 		else if(this.owner.tag == "Enemy")
 		{
-			this.damage = this.owner.GetComponentInChildren<Enemy>().damage;
+			this.damage = this.owner.GetComponentInChildren<Enemy>().damage * Attack.enemy_Damage_Multiplier;
 		}
 	}
 
